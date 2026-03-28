77d0e010dffb174d295f957ff5852db4d7d69589
lemon <lemonwork095@gmail.com>
2026-03-28 20:16:34 +0800

    fix

--- migrate_gameresult_remote.go
+++ migrate_gameresult_remote.go
@@ -23,7 +23,7 @@ const migrateBatchSize = 500
 
 // 每个 fb 最多迁移条数（按 id 升序；源不足则只迁现有条数）
 const (
-	migrateMaxRowsFB0 = 5000
+	migrateMaxRowsFB0 = 2000
 	migrateMaxRowsFB1 = 500
 	migrateMaxRowsFB2 = 500
 )
